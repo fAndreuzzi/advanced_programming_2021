0b0496923fcb0cf8ec7a64735b29676514b579e3
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-01-21 12:42:08 +0100

    constexpr. improve comments

--- exam/stack_pool.hpp
+++ exam/stack_pool.hpp
@@ -16,34 +16,54 @@ class stack_iterator {
 
   stack_iterator(stack_type head, P* pool_ptr)
       : current_head{head}, pool{pool_ptr} {
+    // check the pool
     if (pool_ptr == nullptr)
       throw std::invalid_argument("The given pool points to nullptr.");
+
+    // check the head
+    if (head != pool_ptr->end()) {
+      // this is going to throw an exception in case the given head is
+      // invalid.
+      *(*this);
+    }
   }
   stack_iterator(stack_iterator& other)
-      : current_head(other.current_head), pool{other.pool} {}
+      // delegating constructor
+      : stack_iterator(other.current_head, other.pool) {}
   stack_iterator(stack_iterator&& other)
-      : current_head(other.current_head), pool{other.pool} {}
+      : stack_iterator(other.current_head, other.pool) {
+    other.current_head = other.pool->end();
+    other.pool = nullptr;
+  }
 
-  stack_iterator& operator=(stack_iterator& other) {
+  stack_iterator& operator=(stack_iterator& other) noexcept {
     current_head = other.current_head;
     pool = other.pool;
     return *this;
   }
 
-  stack_iterator& operator=(stack_iterator&& other) {
+  stack_iterator& operator=(stack_iterator&& other) noexcept {
     current_head = std::move(other.current_head);
     pool = other.pool;
+
+    other.current_head = other.pool->end();
     other.pool = nullptr;
+
     return *this;
   }
 
-  // we do not need to do anything since we do not hold the given pool
-  ~stack_iterator() {}
+  // we do not need to do anything since we do not hold the pool, someone else
+  // has to delete that instance.
+  ~stack_iterator() noexcept {}
 
-  T& operator*() const {
+  // we're sure that this is non-throwing if the pool is not modified during
+  // a visit.
+  T& operator*() const noexcept(std::is_const<P>::value) {
     return pool->value(current_head);
   }
 
+  // this throws an exception if current_head does not have a next (i.e. it is
+  // pool.end)
   stack_iterator& operator++() {
     current_head = pool->next(current_head);
     return *this;
@@ -54,12 +74,14 @@ class stack_iterator {
     return tmp;
   }
 
-  stack_type ptr_to_stack() { return current_head; }
+  const stack_type& ptr_to_stack() const noexcept { return current_head; }
 
-  friend bool operator==(const stack_iterator& a, const stack_iterator& b) {
+  friend bool operator==(const stack_iterator& a,
+                         const stack_iterator& b) noexcept {
     return a.current_head == b.current_head;
   }
-  friend bool operator!=(const stack_iterator& a, const stack_iterator& b) {
+  friend bool operator!=(const stack_iterator& a,
+                         const stack_iterator& b) noexcept {
     return !(a == b);
   }
 };
@@ -79,6 +101,7 @@ class stack_iterator {
  * behaviors and unpredictable errors, since elements popped from a stack are
  * not reset to default values (but are inserted into a stack of "free nodes").
  *
+ *
  * @tparam T Type of the values to be held in the stacks of this pool.
  * @tparam N Type using to designate the head of a stack.
  */
@@ -97,11 +120,12 @@ class stack_pool {
 
   stack_type free_nodes;
 
-  node_t& node(stack_type x) noexcept { return pool[x - 1]; }
-  const node_t& node(stack_type x) const noexcept { return pool[x - 1]; }
+  node_t& node(stack_type x) { return pool.at(x - 1); }
+  const node_t& node(stack_type x) const { return pool.at(x - 1); }
 
   template <typename X>
   stack_type _push(X&& val, stack_type head) {
+    // if needed, we allocate a new free node
     if (free_nodes == end()) {
       node_t n = node_t();
       n.next = end();
@@ -125,28 +149,28 @@ class stack_pool {
    * @brief Construct a new stack pool object having initial capacity 0.
    *
    */
-  stack_pool() : free_nodes{end()} {};
+  stack_pool() noexcept : free_nodes{end()} {};
 
   /**
    * @brief Construct a new stack pool object having a given initial capacity.
    *
    * @param n The initial capacity of the pool.
    */
-  explicit stack_pool(size_type n) : free_nodes{end()} { reserve(n); }
+  explicit stack_pool(size_type n) noexcept : free_nodes{end()} { reserve(n); }
 
   using iterator = stack_iterator<stack_type, T, stack_pool>;
   using const_iterator = stack_iterator<stack_type, const T, const stack_pool>;
 
   iterator begin(stack_type x) { return iterator(x, this); }
-  iterator end(stack_type x) { return iterator(end(), this); }
+  iterator end(stack_type x) noexcept { return iterator(end(), this); }
 
   const_iterator begin(stack_type x) const { return const_iterator(x, this); }
-  const_iterator end(stack_type x) const {
-    return const_iterator(end(), *this);
+  const_iterator end(stack_type x) const noexcept {
+    return const_iterator(end(), this);
   }
 
   const_iterator cbegin(stack_type x) const { return const_iterator(x, this); }
-  const_iterator cend(stack_type x) const {
+  const_iterator cend(stack_type x) const noexcept {
     return const_iterator(end(), this);
   }
 
@@ -156,13 +180,15 @@ class stack_pool {
    *
    * @return stack_type
    */
-  stack_type new_stack() { return end(); }
+  stack_type new_stack() noexcept { return end(); }
 
   /**
    * @brief Advise the pool to allocate some more space.
    *
    * This method might be useful to improve performance when adding multiple
    * elements all in once to the pool.
+   *
+   * This method throws an exception if n > vector::max_size.
    *
    * @param n The advised new size of the stack.
    */
@@ -174,7 +200,7 @@ class stack_pool {
    *
    * @return size_type
    */
-  size_type capacity() const { return pool.capacity(); }
+  size_type capacity() const noexcept { return pool.capacity(); }
 
   /**
    * @brief Check whether the given stack is empty.
@@ -183,19 +209,66 @@ class stack_pool {
    * @return true If the stack is empty.
    * @return false Otherwise.
    */
-  bool empty(stack_type x) const { return x == end(); }
+  bool empty(stack_type x) const noexcept { return x == end(); }
 
+  /**
+   * @brief Common end (i.e. after last node) of all the stacks in this pool.
+   *
+   * @return stack_type
+   */
   stack_type end() const noexcept { return stack_type(0); }
 
+  /**
+   * @brief Return the front value in the given stack.
+   *
+   * This method throws an exception if the given head is not a invalid index
+   * in the pool.
+   *
+   * @param x
+   * @return T&
+   */
   T& value(stack_type x) { return node(x).value; }
+  /**
+   * @brief Return the front value in the given stack.
+   *
+   * This method throws an exception if the given head is not a invalid index
+   * in the pool.
+   *
+   * @param x
+   * @return T&
+   */
   const T& value(stack_type x) const { return node(x).value; }
 
+  /**
+   * @brief Return the next node in the given stack.
+   *
+   * This method throws an exception if the given head is not a invalid index
+   * in the pool.
+   *
+   * @param x
+   * @return stack_type&
+   */
   stack_type& next(stack_type x) { return node(x).next; }
+  /**
+   * @brief Return the next node in the given stack.
+   *
+   * This method throws an exception if the given head is not a invalid index in
+   * the pool.
+   *
+   * @param x
+   * @return const stack_type&
+   */
   const stack_type& next(stack_type x) const { return node(x).next; }
 
   /**
    * @brief Push an element to the front of the stack. Returns the new head of
    * the stack.
+   *
+   * This method throws an exception if the given head is not a invalid index in
+   * the pool (i.e. negative index or bigger than the size of the pool).
+   *
+   * The pool does not check if the given head is actually the head of a stack,
+   * therefore it is up to the user to use the pool properly.
    *
    * @param val Value to be pushed.
    * @param head Head of the stack.
@@ -207,6 +280,12 @@ class stack_pool {
    * @brief Push an element to the front of the stack. Returns the new head of
    * the stack.
    *
+   * This method throws an exception if the given head is not a invalid index
+   * in the pool (i.e. negative index or bigger than the size of the pool).
+   *
+   * The pool does not check if the given head is actually the head of a stack,
+   * therefore it is up to the user to use the pool properly.
+   *
    * @param val Value to be pushed.
    * @param head Head of the stack.
    * @return stack_type
@@ -217,6 +296,14 @@ class stack_pool {
 
   /**
    * @brief Pop the head of the given stack.
+   *
+   * This method throws an exception if the given head is not a invalid index
+   * in the pool (i.e. negative index or bigger than the size of the pool).
+   *
+   * The pool does not check if the given head is actually the head of a stack,
+   * therefore it is up to the user to use the pool properly. In case a non-head
+   * index is popped, a kind-of memory leak occur, since the front part of the
+   * stack becomes de-facto unreachable by the pool.
    *
    * @param head Head of the stack from which we intend to pop the front
    *                element.
@@ -234,6 +321,12 @@ class stack_pool {
 
   /**
    * @brief Empty the given stack.
+   *
+   * This method throws an exception if the given head is not a invalid index in
+   * the pool (i.e. negative index or bigger than the size of the pool).
+   *
+   * The pool does not check if the given head is actually the head of a stack,
+   * therefore it is up to the user to use the pool properly.
    *
    * @param head Head of the stack to be emptied.
    * @return stack_type
@@ -261,7 +354,14 @@ class stack_pool {
 
 namespace stack_utils {
   /**
-   * @brief
+   * @brief Push all the items in the given iterator to the given stack (first
+   * to last).
+   *
+   * This function throws an exception if the given head is not a invalid index
+   * in the pool (i.e. negative index or bigger than the size of the pool).
+   *
+   * The pool does not check if the given head is actually the head of a stack,
+   * therefore it is up to the user to use the pool properly.
    *
    * @tparam foreign_iterator Some kind of iterator which delivers values of
    *            type `value_type`.
@@ -287,8 +387,14 @@ namespace stack_utils {
   }
 
   /**
-   * @brief Convert the given stack to a Vector. The stack is empty afterwards,
-   * and should not be mentioned anymore.
+   * @brief Convert the given stack to an std::vector. The stack is empty
+   * afterwards, and should not be mentioned anymore.
+   *
+   * This function throws an exception if the given head is not a invalid index
+   * in the pool (i.e. negative index or bigger than the size of the pool).
+   *
+   * The pool does not check if the given head is actually the head of a stack,
+   * therefore it is up to the user to use the pool properly.
    *
    * @tparam value_type Type of the values held in the stack pool.
    * @tparam stack_type Type of "pointers" to stack nodes.
@@ -312,6 +418,11 @@ namespace stack_utils {
    * @brief Compute the size of the stack starting at the given `head`. The
    * stack is not modified.
    *
+   * This function throws an exception if the given head is not a invalid index
+   * in the pool (i.e. negative index or bigger than the size of the pool).
+   *
+   * This function may also be used to find the size of a portion of a stack.
+   *
    * @tparam value_type Type of the values held in the stack pool.
    * @tparam stack_type Type of "pointers" to stack nodes.
    * @param pool Stack pool containing the stack referenced by `head`.
@@ -319,8 +430,8 @@ namespace stack_utils {
    * @return std::size_t
    */
   template <typename value_type, typename stack_type>
-  std::size_t stack_size(stack_pool<value_type, stack_type>& pool,
-                         stack_type head) {
+  std::size_t stack_size(const stack_pool<value_type, stack_type>& pool,
+                         const stack_type head) {
     std::size_t size = 0;
     for (auto it = pool.cbegin(head); it != pool.cend(head); ++it)
       ++size;
@@ -330,13 +441,18 @@ namespace stack_utils {
   /**
    * @brief Print the content of the given stack. The stack is not modified.
    *
+   * This function throws an exception if the given head is not a invalid index
+   * in the pool (i.e. negative index or bigger than the size of the pool).
+   *
+   * This function may also be used to print only a portion of a stack.
+   *
    * @param os An output stream (like `std::cout` or `std::cerr`).
    * @param head The head of the stack to be printed.
    */
   template <typename value_type, typename stack_type>
   void print_stack(std::ostream& os,
-                   stack_pool<value_type, stack_type>& pool,
-                   stack_type head) {
+                   const stack_pool<value_type, stack_type>& pool,
+                   const stack_type head) {
     os << "STACK (head=" << head << ")" << std::endl;
     for (auto it = pool.cbegin(head); it != pool.cend(head); ++it)
       os << it.ptr_to_stack() << " -> " << *it << std::endl;
