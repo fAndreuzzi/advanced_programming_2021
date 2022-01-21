6dc1f2594babffdc016ae3367a6497fc6d77a781
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-01-21 14:32:23 +0100

    add more tests

--- exam/tests.cpp
+++ exam/tests.cpp
@@ -1,17 +1,16 @@
 #include "catch.hpp"
 
 #include "stack_pool.hpp"
-#include <algorithm> // max_element, min_element
+#include <algorithm>  // max_element, min_element
+#include <vector>
 
-SCENARIO("getting confident with the addresses"){
+SCENARIO("getting confident with the addresses") {
   stack_pool<int, std::size_t> pool{16};
   auto l = pool.new_stack();
 
-  REQUIRE( l == 0);
-  l = pool.push(42,l);
-  REQUIRE( l == std::size_t(1));
-  
-
+  REQUIRE(l == 0);
+  l = pool.push(42, l);
+  REQUIRE(l == std::size_t(1));
 }
 
 SCENARIO("basic operations on stack") {
@@ -20,23 +19,22 @@ SCENARIO("basic operations on stack") {
 
     GIVEN("a new stack") {
       auto l = pool.new_stack();
-      
+
       THEN("the stack is empty")
-	REQUIRE(pool.empty(l));
+      REQUIRE(pool.empty(l));
 
       WHEN("we add one value") {
         l = pool.push(1, l);
         REQUIRE(pool.value(l) == 1);
-      
 
-	WHEN("we delete the only node") {
-	  l = pool.pop(l);
-	  THEN("the stack is empty again")
-	    REQUIRE(pool.empty(l));
+        WHEN("we delete the only node") {
+          l = pool.pop(l);
+          THEN("the stack is empty again")
+          REQUIRE(pool.empty(l));
 
-	  //empty stacks point to pool.end()
-	  REQUIRE(l == pool.end());
-	}
+          // empty stacks point to pool.end()
+          REQUIRE(l == pool.end());
+        }
       }
 
       WHEN("we add two values") {
@@ -125,8 +123,8 @@ SCENARIO("using nodes of a deleted stack") {
   }
 }
 
-SCENARIO("using iterators"){
-  GIVEN("two stacks"){
+SCENARIO("using iterators") {
+  GIVEN("two stacks") {
     stack_pool<int, uint16_t> pool{};
     auto l1 = pool.new_stack();
     // credits: pi as random number generator :)
@@ -141,7 +139,7 @@ SCENARIO("using iterators"){
     l1 = pool.push(5, l1);
     l1 = pool.push(3, l1);
     l1 = pool.push(5, l1);
-    
+
     auto l2 = pool.new_stack();
     l2 = pool.push(8, l2);
     l2 = pool.push(9, l2);
@@ -155,17 +153,96 @@ SCENARIO("using iterators"){
     l2 = pool.push(9, l2);
     l2 = pool.push(7, l2);
 
-    THEN("find the max on l1"){
+    THEN("find the max on l1") {
       auto m = std::max_element(pool.begin(l1), pool.end(l1));
       REQUIRE(*m == 9);
     }
 
-    THEN("find the min on l2"){
+    THEN("find the max on l1 with const iterator") {
+      const auto cpool = pool;
+      stack_iterator<uint16_t, const int, const stack_pool<int, uint16_t>> m =
+          std::max_element(cpool.begin(l1), cpool.end(l1));
+      REQUIRE(*m == 9);
+    }
+
+    THEN("find the max on l1 with c-iterator") {
+      auto m = std::max_element(pool.cbegin(l1), pool.cend(l1));
+      REQUIRE(*m == 9);
+    }
+
+    THEN("find the min on l2") {
       auto m = std::min_element(pool.begin(l2), pool.end(l2));
       REQUIRE(*m == 1);
     }
-    
-    
+
+    THEN("find the min on l2 with const iterator") {
+      const auto cpool = pool;
+      stack_iterator<uint16_t, const int, const stack_pool<int, uint16_t>> m =
+          std::min_element(cpool.begin(l2), cpool.end(l2));
+      REQUIRE(*m == 1);
+    }
+
+    THEN("find the min on l2 with c-iterator") {
+      auto m = std::min_element(pool.cbegin(l2), pool.cend(l2));
+      REQUIRE(*m == 1);
+    }
+  }
+}
+
+SCENARIO("stack_utils namespace") {
+  stack_pool<int, std::size_t> pool;
+  auto l = pool.new_stack();
+  auto l2 = pool.new_stack();
+
+  std::vector<int> v;
+  v.push_back(10);
+  v.push_back(20);
+  v.push_back(10);
+  v.push_back(30);
+
+  THEN("push_all pushes everything") {
+    l = stack_utils::push_all(pool, l, v.begin(), v.end());
+    REQUIRE(pool.value(4) == 30);
+    REQUIRE(pool.value(3) == 10);
+    REQUIRE(pool.value(2) == 20);
+    REQUIRE(pool.value(1) == 10);
+    REQUIRE(!pool.empty(l));
+
+    l = stack_utils::push_all(pool, l, v.begin(), v.end());
+    REQUIRE(pool.value(8) == 30);
+    REQUIRE(pool.value(7) == 10);
+    REQUIRE(pool.value(6) == 20);
+    REQUIRE(pool.value(5) == 10);
+    REQUIRE(pool.value(4) == 30);
+    REQUIRE(pool.value(3) == 10);
+    REQUIRE(pool.value(2) == 20);
+    REQUIRE(pool.value(1) == 10);
+
+    l2 = stack_utils::push_all(pool, l2, v.begin(), v.end());
+    REQUIRE(pool.value(12) == 30);
+    REQUIRE(pool.value(11) == 10);
+    REQUIRE(pool.value(10) == 20);
+    REQUIRE(pool.value(9) == 10);
+    REQUIRE(!pool.empty(l2));
   }
 
+  THEN("to_vector") {
+    l = stack_utils::push_all(pool, l, v.begin(), v.end());
+    l2 = stack_utils::push_all(pool, l2, v.begin(), v.end());
+
+    std::vector<int> v2 = stack_utils::to_vector(pool, l2);
+    REQUIRE(v2.size() == 4);
+    REQUIRE(v2[0] == 30);
+    REQUIRE(v2[1] == 10);
+    REQUIRE(v2[2] == 20);
+    REQUIRE(v2[3] == 10);
+  }
+
+  THEN("stack_size gives correct size") {
+    l = stack_utils::push_all(pool, l, v.begin(), v.end());
+    l = stack_utils::push_all(pool, l, v.begin(), v.end());
+    l2 = stack_utils::push_all(pool, l2, v.begin(), v.end());
+    REQUIRE(stack_utils::stack_size(pool, l2) == 4);
+    REQUIRE(stack_utils::stack_size(pool, l) == 8);
+  }
 }
